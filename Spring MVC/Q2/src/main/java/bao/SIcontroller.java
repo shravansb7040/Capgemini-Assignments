c6600691e5275133feffb4501da5caf714fcf757
Shravan Kumar S Bagewadi <shravansb1234@gmail.com>
2021-02-15 15:05:17 +0530

    Completed spring mvc asssignments

---
+++ Spring MVC/Q2/src/main/java/bao/SIcontroller.java
@@ -0,0 +1,26 @@
+package bao;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import scsbean.SI;
+
+@Controller
+public class SIController {
+    @RequestMapping(value = "/si", method = RequestMethod.GET)
+      public ModelAndView siload() {
+        return new ModelAndView("siform", "command", new SI());
+       }
+    @RequestMapping(value = "/silogic", method = RequestMethod.POST)
+      public ModelAndView silogic(@ModelAttribute("SpringWeb")SI s, ModelMap model) {
+         model.addAttribute("p", s.getP());
+         model.addAttribute("r", s.getR());
+         model.addAttribute("t", s.getT());
+         float s1 = (s.getP()*s.getR()*s.getT())/100;
+         return new ModelAndView("siresult","res",s1);
+       }
+}
