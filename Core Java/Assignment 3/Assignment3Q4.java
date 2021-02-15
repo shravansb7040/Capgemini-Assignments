0e30b1df4de9514156b58621dcb86c4f69ba0d7a
Shravan Kumar S Bagewadi <shravansb1234@gmail.com>
2021-02-15 14:58:03 +0530

    Completed remaining core java and spring core assignments

--- Core Java/Assignment 3/Assignment3Q4.java
+++ Core Java/Assignment 3/Assignment3Q4.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 
 class DateClass {
     private int date;
     private int month;
     private int year;
-    private String dob;
     
     public DateClass(int date, int month, int year)
     {
@@ -23,19 +23,9 @@ class DateClass {
     	return this.date;
     }
     
-    public void setDate(int date)
-    {
-    	this.date=date;
-    }
-    
     public int getMonth()
     {
     	return this.month;
-    }
-    
-    public void setMonth(int month)
-    {
-    	this.month=month;
     }
     
     public int getYear()
@@ -43,99 +33,78 @@ class DateClass {
     	return this.year;
     }
     
-    public void setYear(int year)
-    {
-    	this.year=year;
-    }
-    
     public String getDOB()
     {
     	return this.date+"/"+this.month+"/"+this.year;
     }
     
-    public void setDOB(String dob)
-    {
-    	this.dob=dob;
-    }
+    @Override 
+    public int hashCode() { 
+      final int number = 13; 
+      int result = 3;
+	  result = number * result + date; 
+	  result = number * result + month; 
+	  // result =prime * result + year; 
+	  return result; }
+	
+	@Override 
+	public boolean equals(Object obj) { 
+      if (this == obj) 
+    	  return true;
+	  if (obj == null) 
+		  return false; 
+	  if (getClass() != obj.getClass()) 
+		  return false; 
+	  DateClass other = (DateClass) obj; 
+	  if (date != other.date) 
+		  return false; 
+	  if (month != other.month) 
+		  return false;  
+	  if (year == other.year) 
+		  return false;
+		  
+	  return true; 
+	}
     
-    @Override
-    public boolean equals(Object o) 
-    { 
-        if (this == o) { 
-            return true; 
-        } 
-        if (o == null) { 
-            return false; 
-        } 
-        if (this.getClass() != o.getClass()) { 
-            return false; 
-        } 
-        DateClass other = (DateClass)o; 
-        if (this.date == other.date && this.month==other.month) { 
-            return false; 
-        } 
-        return true; 
-    } 
 }
 
 public class Assignment3Q4 {
 
-    public void getEmployee(HashMap<DateClass, String> dob,String employeeName)
+    public String getEmployee(HashMap<DateClass, String> dob,String employeeName)
     {
-    	HashMap<String,String> Employees=new HashMap<>();
-    	String[] employeeNames=employeeName.split(",");
-    	String[] values=new String[employeeNames.length];
-    	String value="";
-    	Iterator trav=dob.entrySet().iterator();
-    	while(trav.hasNext())
-    	{
-    		Map.Entry record=(Map.Entry)trav.next();
-    		String name=(String) record.getValue();
-    		value+=name+" ";
-    	}
-    	
-    	values=value.split(" ");
-    	Arrays.sort(values);
-    	for(int i=0;i<4;i++)
-    	{
-    		Employees.put(values[i], employeeNames[i]);	
-    	}
-    	System.out.println(Employees);
+    	String s="get method fails";
+        
+        for (Map.Entry<DateClass, String> entry : dob.entrySet()) {
+        	System.out.println(entry.getValue());
+        	System.out.println(employeeName);
+            if(entry.getValue().equals(employeeName))
+               { 
+            	 s=entry.getValue();
+            	 break;
+               }
+        }
+        return s;
 	}
     public static void main(String[] args) 
     {
+    	Scanner scan=new Scanner(System.in);
     	HashMap<DateClass, String> dob = new HashMap<>();
     	DateClass one=new DateClass(28,2,1999);
     	DateClass two=new DateClass(14,6,1989);
-    	DateClass three=new DateClass(28,2,1998);
-    	DateClass four=new DateClass(20,1,1999);
+    	DateClass three=new DateClass(28,3,1998);
+    	DateClass four=new DateClass(14,6,1999);
     	
-    	dob.put(one, one.getDOB());
-    	dob.put(two,two.getDOB());
-    	dob.put(three, three.getDOB());
-    	dob.put(four, four.getDOB());
+    	dob.put(one, "one");
+    	dob.put(two,"two");
+    	dob.put(three, "three");
+    	dob.put(four, "four");
     	
-    	String employeeName="Arun,Bharath,Chandra,Dinesh";
-    	Assignment3Q4 answer=new Assignment3Q4();
-    	
-    	for(Map.Entry<DateClass,String> entry: dob.entrySet())
-        {
-    		for(Map.Entry<DateClass, String> entry1: dob.entrySet())
-    		{
-    			if (!entry.getKey().equals(entry1.getKey())) 
-    	           {
-    	             if (!entry.getValue().equals(entry1.getValue())) {
-    	            	 answer.getEmployee(dob, employeeName);
-    	            	 break;
-    	             }
-    	             else
-    	             {
-    	            	 System.exit(0);
-    	             }
-    	             break;
-    	           }
-    			break;
-    		}
-        }
+        System.out.println("hashmap : "+dob);
+        
+        System.out.print("enter the name of employee you want to check : ");
+        String name=scan.next();
+        
+        Assignment3Q4 answer=new Assignment3Q4();
+        System.out.println("          "+answer.getEmployee(dob,name));
     }
 }
